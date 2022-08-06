472d14f2567509cd432d2a4267f9e754662e5469
mrkresnofatih <mrkresnofatih@gmail.com>
2022-08-07 05:18:51 +0700

    update color utils & add readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# PyGithubStatsGenerator
+
+Create Github Stats Generator using Python, Plotly, & Docker.
+
+## How to use
+
+1. Generate personal access token [here](https://github.com/settings/tokens).
+2. Run:
+```yaml
+docker run --name pygithubstatsgenerator -e APP_GH_TOKEN=<personal_access_token> -e APP_TARGET_REPONAME=<target_public_repo_that_you_own> ghcr.io/mrkresnofatih/ghcr.io/mrkresnofatih/pyghstatsgenerator:v1.0.2
+```
--- colorutils.py
+++ colorutils.py
@@ -1,12 +1,12 @@
 import random
 
 def get_random_color_palette():
-    randomnumber = random.uniform(0, 100)
-    if randomnumber < 25:
-        return ['#f94144', '#f3722c', '#f8961e', '#f9844a', '#f9c74f', '#90be6d', '#43aa8b', '#4d908e', '#577590', '#277da1']
-    elif randomnumber >= 25 and randomnumber < 50:
-        return ["#ff5400","#ff6d00","#ff8500","#ff9100","#ff9e00","#00b4d8","#0096c7","#0077b6","#023e8a","#03045e"]
-    elif randomnumber >= 50 and randomnumber < 75:
-        return ["#03071e","#370617","#6a040f","#9d0208","#d00000","#dc2f02","#e85d04","#f48c06","#faa307","#ffba08"]
-    elif randomnumber >= 75 and randomnumber <= 100:
-        return ["#7400b8","#6930c3","#5e60ce","#5390d9","#4ea8de","#48bfe3","#56cfe1","#64dfdf","#72efdd","#80ffdb"]
+    colorpalettes = [
+        ['#f94144', '#f3722c', '#f8961e', '#f9844a', '#f9c74f', '#90be6d', '#43aa8b', '#4d908e', '#577590', '#277da1'],
+        ["#001219", "#005f73", "#0a9396", "#94d2bd", "#e9d8a6", "#ee9b00", "#ca6702", "#bb3e03", "#ae2012", "#9b2226"],
+        ["#012a4a", "#013a63", "#01497c", "#014f86", "#2a6f97", "#2c7da0", "#468faf", "#61a5c2", "#89c2d9", "#a9d6e5"],
+        ["#03071e", "#370617", "#6a040f", "#9d0208", "#d00000", "#dc2f02", "#e85d04", "#f48c06", "#faa307", "#ffba08"],
+        ["#7400b8", "#6930c3", "#5e60ce", "#5390d9", "#4ea8de", "#48bfe3", "#56cfe1", "#64dfdf", "#72efdd", "#80ffdb"]
+    ]
+    random.shuffle(colorpalettes)
+    return colorpalettes[0]
